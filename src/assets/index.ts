532b18a8a13b615af11637293562b649644dfda0
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-31 00:13:12 +0700

    feat: add images for project

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,23 @@
+import anhbia from './anhbia.jpg';
+import avatar1 from './avatar1.jpg';
+import avatar2 from './avatar2.jpg';
+import avataa from './avatvata.jpeg';
+import design from './design.png';
+import develop from './develop.png';
 import facebook_logo from './Facebook_logo.png.webp';
+import game from './game.png';
+import healt from './healt.svg';
+import menu from './menu.png';
 
-export { facebook_logo };
+export {
+  facebook_logo,
+  anhbia,
+  avataa,
+  avatar1,
+  avatar2,
+  healt,
+  design,
+  game,
+  develop,
+  menu,
+};
