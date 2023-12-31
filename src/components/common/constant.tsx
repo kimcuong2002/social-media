3b2642dea927b8b966bcc6e0c95e40ebfb28f7e3
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-31 21:45:08 +0700

    feat: create optionInput icons

---
+++ src/components/common/constant.tsx
@@ -0,0 +1,9 @@
+import { BiFile, BiImage, BiSend, BiSmile, BiVideo } from 'react-icons/bi';
+
+export const optionInput = [
+  { icon: <BiVideo />, id: 1 },
+  { icon: <BiSmile />, id: 2 },
+  { icon: <BiImage />, id: 3 },
+  { icon: <BiFile />, id: 4 },
+  { icon: <BiSend />, id: 5 },
+];
