3aa3bc7b1846f55842a9c89416d745588f00da07
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-01-03 11:42:55 +0700

    feat: add image for project

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,9 @@
+import anh1 from './anh1a.jpeg';
+import anh2 from './anh2.jpeg';
+import anh3 from './anh3.jpeg';
+import anh4 from './anh4.jpeg';
+import anh5 from './anh5.jpeg';
+import anh6 from './anh6.jpeg';
 import anhbia from './anhbia.jpg';
 import avatar1 from './avatar1.jpg';
 import avatar2 from './avatar2.jpg';
@@ -12,6 +18,12 @@ import icons from './icons.png';
 import menu from './menu.png';
 
 export {
+  anh1,
+  anh2,
+  anh3,
+  anh4,
+  anh5,
+  anh6,
   google,
   icons,
   facebook_logo,
