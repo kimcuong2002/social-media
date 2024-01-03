38c69641a25c0ca10b87f46e11ac680b18c01427
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-01-03 16:14:10 +0700

    create profile list frinds component

---
+++ src/pages/profile/components/list-friend.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react';
+
+import { Box, Divider, Text } from '@chakra-ui/react';
+
+import { Contact } from '@/components';
+import { datas } from '@/data';
+
+const ListFriend = () => {
+  const friends = useMemo(() => {
+    const listFriends = datas.slice(0, 10);
+    return listFriends;
+  }, [datas]);
+
+  return (
+    <Box className="bg-white mt-4 p-4 rounded-lg border-2">
+      <Box className="flex justify-between">
+        <Text className="font-bold">List friends</Text>
+        <Text className="text-green-900 font-bold cursor-pointer">See all</Text>
+      </Box>
+      <Divider className="my-2" />
+      {friends.map((item) => (
+        <Contact
+          key={item.id}
+          name={item.name}
+          state={item.state}
+          thumb={item.thumb}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default ListFriend;
