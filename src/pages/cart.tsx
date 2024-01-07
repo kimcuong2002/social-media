ba0d5c5d4e8628b5629847a7950ddb09a82f32f4
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-01-07 21:39:25 +0700

    feat: create cart page

---
+++ src/pages/cart.tsx
@@ -0,0 +1,86 @@
+import { Box, Grid, Text } from '@chakra-ui/react';
+
+import NavbarLayout from '../layout/navbar-layout/index';
+import { avatar1 } from '@/assets';
+import { SideBar } from '@/features';
+import { Category } from '@/features';
+
+const categoriesDatas = [
+  {
+    id: 1,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 2,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 3,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+
+  {
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 4,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 5,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 6,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+  {
+    id: 7,
+    thumb: avatar1,
+    name: 'Iphone 12',
+    price: 999.99,
+    location: 'Da Nang',
+  },
+];
+
+export const Cart = () => {
+  return (
+    <NavbarLayout navBarChildren={<SideBar />}>
+      <Box className="w-ful mx-10 mt-4">
+        <Text className="text-2xl font-bold my-4">Suggested to you</Text>
+        <Grid templateColumns="repeat(6, 1fr)" gap={6}>
+          {categoriesDatas.map((item) => (
+            <Category
+              name={item.name}
+              key={item.id}
+              thumb={item.thumb}
+              location={item.location}
+              price={item.price}
+            />
+          ))}
+        </Grid>
+      </Box>
+    </NavbarLayout>
+  );
+};
