ec7189d36ccb5ef6d0811f360706f40f1faf7d06
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-01-08 17:29:30 +0700

    fix: fix ui mobile-menu component

--- src/layout/header/components/mobile-menu.tsx
+++ src/layout/header/components/mobile-menu.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState, ReactNode } from 'react';
+
 import {
   Box,
   Button,
@@ -10,26 +12,46 @@ import {
 } from '@chakra-ui/react';
 import { BiMenu } from 'react-icons/bi';
 
+import { CartSideBar, HomeSideBar } from '@/features';
 import LeftNavBar from '@/layout/navbar-layout/components/left-navbar';
 
 const MobileMenu = () => {
+  const [sidebarComponent, setSidebarComponent] = useState<ReactNode>();
+
+  const currentPath = window.location.pathname;
+  useEffect(() => {
+    if (currentPath == '/') {
+      setSidebarComponent(<HomeSideBar />);
+    } else {
+      setSidebarComponent(<CartSideBar />);
+    }
+  }, [currentPath]);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleClick = () => {
     onOpen();
   };
+
   return (
-    <Box>
+    <Box className="lg:hidden">
       <Button px="0" onClick={() => handleClick()}>
         <WrapItem className="bg-gray-500 p-2 rounded-full">
           <BiMenu className="text-md lg:text-xl text-white" />
         </WrapItem>
       </Button>
-      <Drawer onClose={onClose} isOpen={isOpen} size="full" placement="left">
+      <Drawer
+        onClose={onClose}
+        isOpen={isOpen}
+        size={{ xs: 'full', md: 'lg' }}
+        placement="left"
+      >
         <DrawerOverlay />
-        <DrawerContent>
+        <DrawerContent className="!bg-gray-500">
           <DrawerCloseButton />
-          <LeftNavBar />
+          <LeftNavBar className="w-full !flex h-screen">
+            {sidebarComponent}
+          </LeftNavBar>
         </DrawerContent>
       </Drawer>
     </Box>
