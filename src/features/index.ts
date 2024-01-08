cdd65707cc639aa92c8b52125eb18d7f60d36932
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-01-08 17:34:28 +0700

    fix: fix export

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,7 +1,8 @@
 export * from './login/components/signin-form';
 export * from './login/components/login-form';
 
-export * from './cart/components/siddebar';
 export * from './post/post';
 export * from './home/components/right-navbar';
 export * from './cart/components/category';
+export * from './cart/components/cart-sidebar';
+export * from './home/components/home-sidebar';
