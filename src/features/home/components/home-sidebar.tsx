ef8e5a0d3b6f0db6850fcf548e6ce6f18e3ffc7e
kimcuong <nguyenhoangkimcuong02@gmail.com>
2024-04-07 11:03:56 +0700

    feat: fix ui home sidebar

--- src/features/home/components/home-sidebar.tsx
+++ src/features/home/components/home-sidebar.tsx
@@ -1,7 +1,10 @@
+import { useMemo } from 'react';
+
 import { Avatar, Box, Image, Text } from '@chakra-ui/react';
 
-import { avatar1, menu } from '@/assets';
+import { menu } from '@/assets';
 import { healt, design, develop, game } from '@/assets';
+import { useQueryInfoUser } from '@/features/auth';
 
 const topics = [
   {
@@ -51,12 +54,23 @@ const trends = [
 
 export const HomeSideBar = () => {
   const sortTrends = trends.sort((a, b) => b.totalPost - a.totalPost);
+  const { data } = useQueryInfoUser();
+  const userData = useMemo(() => {
+    if (data) {
+      const result = {
+        name: data?.getInfoUser.fullname,
+        avatar: data?.getInfoUser.avatar,
+      };
+      return result;
+    }
+    return null;
+  }, [data]);
   return (
     <Box>
       <Box className="flex items-center gap-2">
-        <Avatar src={avatar1} size="sm" />
+        <Avatar src={userData?.avatar} size="sm" />
         <Text fontSize="sm" className="font-bold">
-          Kim Cuong
+          {userData?.name}
         </Text>
       </Box>
       <Text className="uppercase my-4 text-violet-800 font-bold" fontSize="sm">
