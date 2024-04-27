ed2a27bff5d7783cc516c9eb40b5509da8d9fb9c
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-04-28 02:45:12 +0700

    feat: create comment component

--- src/features/comment/components/comment.tsx
+++ src/features/comment/components/comment.tsx
@@ -1,19 +1,48 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 
-import { Avatar, Box, Text } from '@chakra-ui/react';
-import { BiHeart } from 'react-icons/bi';
+import { Avatar, Box, Img, Text } from '@chakra-ui/react';
+import { CommentType } from '../service';
+import { durationTime } from '@/utils';
+import { CreateReplyCmt } from './create-reply-comment';
 
-import CommentInput from './comment-input';
-
-export const Comment = () => {
-  const [reply, setReply] = useState(false);
-
-  const toggleOpenReply = () => {
-    setReply(!reply);
-  };
+export const Comment: FC<CommentType> = ({
+  author,
+  content,
+  images,
+  videos,
+  createdAt,
+  id,
+  postId,
+}) => {
+  const [repCmt, setRepCmt] = useState(false);
   return (
-    <Box className="flex gap-2">
-     
+    <Box className="flex gap-2 my-4">
+      <Avatar src={author?.avatar} />
+      <Box>
+        <Box className="bg-[#f0f0f1] py-1 px-2 rounded-lg">
+          <Text className="font-medium">{author?.fullname}</Text>
+          <Text>{content}</Text>
+        </Box>
+        <Box className="columns-2 mt-2">
+          {images?.map((img) => (
+            <Img src={img} alt="" key={img} className="rounded-lg" />
+          ))}
+        </Box>
+        <Box className="columns-2">
+          {videos?.map((video) => <Img src={video} alt="" key={video} />)}
+        </Box>
+        <Box className="flex gap-2 text-xs my-2">
+          <Text>{durationTime(createdAt)}</Text>
+          <Text className="font-bold cursor-pointer">Like</Text>
+          <Text
+            className="font-bold cursor-pointer"
+            onClick={() => setRepCmt(!repCmt)}
+          >
+            Reply
+          </Text>
+        </Box>
+        {repCmt && <CreateReplyCmt idCmtParent={id} postId={postId} />}
+      </Box>
     </Box>
   );
 };
