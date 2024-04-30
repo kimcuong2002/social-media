109c1bdf791b7892576f8ceb95cc3387341fcfa0
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-04-30 14:36:58 +0700

    feat: create pagination constant data

--- src/data/constant/pagination.ts
+++ src/data/constant/pagination.ts
@@ -1,3 +1,8 @@
+import { useQueryInfoUser } from '@/features/auth';
+
+const { data: userData } = useQueryInfoUser();
+
 export const LIMIT = 10;
 export const PAGE = 1;
 export const FILTER = {};
+export const ID_USER = userData?.getInfoUser.id as string;
