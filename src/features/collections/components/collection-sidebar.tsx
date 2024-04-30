a5d87afa95f110975c744c415cf93d8a2b958349
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-04-30 23:42:29 +0700

    fix: fix ui collection sidebar

--- src/features/collections/components/collection-sidebar.tsx
+++ src/features/collections/components/collection-sidebar.tsx
@@ -1,38 +1,43 @@
-import { Box, Text, Divider, Avatar } from '@chakra-ui/react';
-import { LuTags } from 'react-icons/lu';
-import { MdOutlineEventBusy } from 'react-icons/md';
-import { RiCompassDiscoverLine } from 'react-icons/ri';
+import {
+  Box,
+  Text,
+  Divider,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 import { SideBarButton } from '@/components';
 import { CreateCollection } from './create-collection';
 import { useGetCollections, useGetSaved } from '../hooks/use-collections-query';
 import { useMemo, useState } from 'react';
 import { LIMIT, PAGE } from '@/data';
-
-const eventSideBar = [
-  {
-    name: 'Discover Event',
-    icon: <RiCompassDiscoverLine />,
-  },
-  {
-    name: 'Your Event',
-    icon: <MdOutlineEventBusy />,
-  },
-  {
-    name: 'Saved Event',
-    icon: <LuTags />,
-  },
-];
+import { CiEdit, CiTrash, CiMenuBurger } from 'react-icons/ci';
+import DeleteCollection from './delete-collection';
+import EditCollection from './edit-collection';
+import { eventSideBar } from '../service/constant';
 
 export const CollectionSideBar = () => {
   const [page] = useState(PAGE);
+  const [actionsCollection, setActionsCollection] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { data: getSaved } = useGetSaved();
   const idSaved = useMemo(() => {
     return getSaved?.getSaved?.id;
   }, [getSaved]);
 
-  const { data: getCollections } = useGetCollections(
+  const { data: getCollections, refetch } = useGetCollections(
     idSaved as string,
     LIMIT,
     page,
@@ -41,7 +46,7 @@ export const CollectionSideBar = () => {
   return (
     <>
       <Text className="font-bold" fontSize="2xl">
-        Event
+        Collections
       </Text>
       <Divider className="my-4" />
       <Box className="flex flex-col">
@@ -52,7 +57,6 @@ export const CollectionSideBar = () => {
             buttonName={item.name}
           />
         ))}
-        <CreateCollection />
       </Box>
       <Divider className="my-4" />
       <Text className="uppercase my-4 text-violet-800 font-bold" fontSize="sm">
@@ -61,13 +65,76 @@ export const CollectionSideBar = () => {
       {getCollections?.getCollections.data.map((item) => (
         <Box
           key={item.id}
-          className="flex items-center gap-2 my-2 cursor-pointer hover:bg-slate-200 p-2 rounded-xl"
+          className="flex justify-between items-center my-2 cursor-pointer hover:bg-slate-200 p-2 rounded-xl"
         >
-          <Avatar src={item.avatar} />
-          <></>
-          <Text className="font-bold">{item.name}</Text>
+          <Box className="flex items-center gap-2">
+            <Avatar src={item.avatar} />
+            <Text className="font-bold">{item.name}</Text>
+          </Box>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<CiMenuBurger className="text-xl" />}
+              variant="outline"
+            />
+            <MenuList>
+              <MenuItem
+                icon={<CiEdit className="text-xl" />}
+                command="⌘E"
+                onClick={() => {
+                  onOpen();
+                  setActionsCollection('edit');
+                }}
+              >
+                Edit Collection
+              </MenuItem>
+              <MenuItem
+                icon={<CiTrash className="text-xl" />}
+                command="⌘D"
+                onClick={() => {
+                  onOpen();
+                  setActionsCollection('del');
+                }}
+              >
+                Delete Collection
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>
+                {actionsCollection === 'del'
+                  ? 'Delete Collection'
+                  : 'Edit Collection'}
+              </ModalHeader>
+              <Divider />
+              <ModalCloseButton />
+              <ModalBody>
+                {actionsCollection === 'del' ? (
+                  <DeleteCollection
+                    idCollection={item.id}
+                    name={item.name}
+                    avatar={item.avatar}
+                    refetch={() => refetch()}
+                    onClose={onClose}
+                  />
+                ) : (
+                  <EditCollection
+                    idCollection={item.id}
+                    name={item.name}
+                    avatar={item.avatar}
+                    refetch={() => refetch()}
+                    onClose={onClose}
+                  />
+                )}
+              </ModalBody>
+            </ModalContent>
+          </Modal>
         </Box>
       ))}
+      <CreateCollection refetch={refetch} />
       <Divider className="my-4" />
     </>
   );
