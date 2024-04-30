23bfa61ffe89856accb945f0ed5ccdc9fa57dda9
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-04-30 23:45:58 +0700

    feat: create event sideBar and groupSideBar constant

---
+++ src/features/group/service/constant.tsx
@@ -0,0 +1,16 @@
+import { CiLocationArrow1, CiLock, CiUnlock } from 'react-icons/ci';
+
+export const groupSideBarButton = [
+  {
+    name: 'Group Feeds',
+    icon: <CiLocationArrow1 />,
+  },
+  {
+    name: 'Discover Group',
+    icon: <CiLock />,
+  },
+  {
+    name: 'Notifcations',
+    icon: <CiUnlock />,
+  },
+];
