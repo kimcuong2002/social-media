735754a740f4ffeb1eb048a2283bbb5461260b8b
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-04-30 14:36:43 +0700

    feat: create pagination constant data

---
+++ src/data/constant/pagination.ts
@@ -0,0 +1,3 @@
+export const LIMIT = 10;
+export const PAGE = 1;
+export const FILTER = {};
--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ export * from './constant/path';
 export * from './constant/color';
 export * from './constant/user';
 export * from './constant/path';
+export * from './constant/pagination';
