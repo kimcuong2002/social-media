76223e5eaf30be60d9d190dbc8fb82cec60be555
kimcuong2002 <nguyenhoangkimcuong02@gmail.com>
2024-05-23 18:07:56 +0700

    fix: fix logo component

--- src/layout/header/components/logo.tsx
+++ src/layout/header/components/logo.tsx
@@ -1,16 +1,16 @@
-import { Avatar, Box, Text } from '@chakra-ui/react';
+import { Avatar, Box, Image, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-import { facebook_logo } from '@/assets';
+import { logo } from '@/assets';
 import { HOME_PATH } from '@/data';
 
 const Logo = () => {
   return (
     <Link to={HOME_PATH}>
       <Box className="flex justify-center items-center gap-4 cursor-pointer">
-        <Avatar src={facebook_logo} className="!w-8 !h-8 lg:!w-9 lg:!h-9" />
-        <Text fontSize="lg" className="hidden lg:block">
-          Facebook
+        <Image src={logo} className="w-8 h-8 lg:w-14 lg:!h-14 rounded-lg" />
+        <Text fontSize="lg" className="hidden lg:block font-bold">
+          Social Media
         </Text>
       </Box>
     </Link>
